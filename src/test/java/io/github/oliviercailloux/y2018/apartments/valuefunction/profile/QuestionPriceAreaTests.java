958ffa6aa2fb9f438f65ec901685ce4b93da8058
av1m <36456709+av1m@users.noreply.github.com>
2020-06-29 11:13:03 +0200

    add test with subjective value

--- src/test/java/io/github/oliviercailloux/y2018/apartments/valuefunction/profile/QuestionPriceAreaTests.java
+++ src/test/java/io/github/oliviercailloux/y2018/apartments/valuefunction/profile/QuestionPriceAreaTests.java
@@ -1,8 +1,10 @@
 package io.github.oliviercailloux.y2018.apartments.valuefunction.profile;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import com.google.common.collect.Range;
+import io.github.oliviercailloux.y2018.apartments.apartment.Apartment;
 import io.github.oliviercailloux.y2018.apartments.valuefunction.Criterion;
 import org.junit.jupiter.api.Test;
 
@@ -32,5 +34,41 @@ public class QuestionPriceAreaTests {
     assertEquals(Range.closed(8.1d, 15.0d), profile2.getWeightRange(Criterion.FLOOR_AREA));
     assertEquals(15.0d, profile2.getWeightRange(Criterion.FLOOR_AREA).upperEndpoint(), 0.0001);
     assertEquals(8.1d, profile2.getWeightRange(Criterion.FLOOR_AREA).lowerEndpoint(), 0.0001);
+  }
+
+  /**
+   * Creation of a Student profile. We ask the question: are you ready to pay x € for y m² more? We
+   * assume that the answer is positive. We adapt the linear AVF We consider an apartment a and an
+   * apartment b x € more expensive and with y m² more. We check that b is preferred to a with the
+   * new avf.
+   */
+  @Test
+  void testSubjectiveValue() {
+    QuestionPriceArea question = QuestionPriceArea.create(10, 20);
+    Profile profile = ProfileManager.getInstance().getProfile(ProfileType.STUDENT);
+    Profile profile1 = question.resolve(profile, true);
+    Apartment a =
+        new Apartment.Builder()
+            .setAddress("118 rue du père noel 77480")
+            .setTitle("Grand Igloo")
+            .setTerrace(false)
+            .setWifi(false)
+            .setTele(false)
+            .setPricePerNight(20)
+            .setFloorArea(50.0d)
+            .build();
+    Apartment b =
+        new Apartment.Builder()
+            .setAddress("118 rue du père noel 77480")
+            .setTitle("Grand Igloo")
+            .setTerrace(false)
+            .setWifi(false)
+            .setTele(false)
+            .setPricePerNight(20 + 10)
+            .setFloorArea(50.0d + 20.0d)
+            .build();
+    assertTrue(
+        profile1.getLinearAVF().getSubjectiveValue(a)
+            < profile1.getLinearAVF().getSubjectiveValue(b));
   }
 }
